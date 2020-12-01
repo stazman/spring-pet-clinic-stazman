f3c12143241d5eb3f5c3beb0fa8135a57ce95096
Christopher DiStasio <cadistasio@gmail.com>
2020-12-01 14:40:50 -0600

    Adds initial project files

---
+++ src/main/java/com/stazman/springpetclinicstazman/SpringPetClinicStazmanApplication.java
@@ -0,0 +1,13 @@
+package com.stazman.springpetclinicstazman;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringPetClinicStazmanApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringPetClinicStazmanApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/stazman/springpetclinicstazman/SpringPetClinicStazmanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stazman.springpetclinicstazman;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringPetClinicStazmanApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
