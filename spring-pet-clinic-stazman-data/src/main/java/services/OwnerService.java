e0d24a631864b9082c9f9d7f432ce4067442923b
Christopher DiStasio <cadistasio@gmail.com>
2020-12-10 22:16:01 -0600

    Adds, completes OwnerService interface

---
+++ spring-pet-clinic-stazman-data/src/main/java/services/OwnerService.java
@@ -0,0 +1,14 @@
+package services;
+
+import models.Owner;
+
+import java.util.Set;
+
+public interface OwnerService {
+
+    Owner findByLastName(String lastName);
+    Owner findById(Long id);
+    Owner save(Owner owner);
+    Set<Owner> findAll();
+
+}
