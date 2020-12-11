ed04b534f82b5612672c8a99b554b7034e8a6aa7
Christopher DiStasio <cadistasio@gmail.com>
2020-12-10 22:24:29 -0600

    Adds, completes VetService interface; closes issue #5

---
+++ spring-pet-clinic-stazman-data/src/main/java/services/VetService.java
@@ -0,0 +1,13 @@
+package services;
+
+import models.Vet;
+
+import java.util.Set;
+
+public interface VetService {
+
+    Vet findById(Long id);
+    Vet save(Vet vet);
+    Set<Vet> findAll();
+
+}
