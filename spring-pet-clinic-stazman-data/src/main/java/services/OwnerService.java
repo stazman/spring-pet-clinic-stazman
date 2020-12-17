e6d861c7f07e271e70e1f194691aa1f23b72be16
Christopher DiStasio <cadistasio@gmail.com>
2020-12-17 02:53:24 -0600

    Refactors OwnerService

--- spring-pet-clinic-stazman-data/src/main/java/services/OwnerService.java
+++ spring-pet-clinic-stazman-data/src/main/java/services/OwnerService.java
@@ -2,13 +2,8 @@ package services;
 
 import models.Owner;
 
-import java.util.Set;
-
-public interface OwnerService {
+public interface OwnerService extends CrudService<Owner, Long> {
 
     Owner findByLastName(String lastName);
-    Owner findById(Long id);
-    Owner save(Owner owner);
-    Set<Owner> findAll();
 
 }
