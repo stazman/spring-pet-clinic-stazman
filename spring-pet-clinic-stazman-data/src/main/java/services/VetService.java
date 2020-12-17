0ad576c0b0a1b8c369f383642dade4b9b5df9f3e
Christopher DiStasio <cadistasio@gmail.com>
2020-12-17 04:19:10 -0600

    Refactors VetService upon extending CrudService; closes #11

--- spring-pet-clinic-stazman-data/src/main/java/services/VetService.java
+++ spring-pet-clinic-stazman-data/src/main/java/services/VetService.java
@@ -2,12 +2,6 @@ package services;
 
 import models.Vet;
 
-import java.util.Set;
-
-public interface VetService {
-
-    Vet findById(Long id);
-    Vet save(Vet vet);
-    Set<Vet> findAll();
+public interface VetService extends CrudService<Vet, Long>{
 
 }
