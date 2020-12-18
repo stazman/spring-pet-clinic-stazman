40c0903325947803574135c29b3901b1d4bac409
Christopher DiStasio <cadistasio@gmail.com>
2020-12-17 19:30:24 -0600

    Corrects directory locations, paths

--- spring-pet-clinic-stazman-web/src/main/java/com/stazman/springpetclinicstazman/SpringPetClinicStazmanApplication.java
+++ spring-pet-clinic-stazman-web/src/main/java/com/stazman/springpetclinicstazman/SpringPetClinicStazmanApplication.java
@@ -1,4 +1,4 @@
-package springpetclinicstazman;
+package com.stazman.springpetclinicstazman;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- spring-pet-clinic-stazman-web/src/test/java/com/stazman/springpetclinicstazman/SpringPetClinicStazmanApplicationTests.java
+++ spring-pet-clinic-stazman-web/src/test/java/com/stazman/springpetclinicstazman/SpringPetClinicStazmanApplicationTests.java
@@ -1,4 +1,4 @@
-package springpetclinicstazman;
+package com.stazman.springpetclinicstazman;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
