8c0dcecee9a873166ffaa17b673243c18bab7d99
Christopher DiStasio <cadistasio@gmail.com>
2020-12-25 12:31:20 -0600

    Refactors model-specific services for data loader

--- spring-pet-clinic-stazman-data/src/main/java/map/VetServiceMap.java
+++ spring-pet-clinic-stazman-data/src/main/java/map/VetServiceMap.java
@@ -1,11 +1,11 @@
 package map;
 
 import models.Vet;
-import services.CrudService;
+import services.VetService;
 
 import java.util.Set;
 
-public class VetServiceMap extends AbstractMapService<Vet, Long> implements CrudService<Vet, Long> {
+public class VetServiceMap extends AbstractMapService<Vet, Long> implements VetService {
 
     @Override
     public Set<Vet> findAll() {
